15190a79a6a51ada9fea9f19e05860e409d0ef57
Karthik Murali <karthik.murali60@gmail.com>
2023-10-13 16:46:59 +0000

    Refactor code and fix issues

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "1.0.0"
     }
   }
+
+  cloud {
+    organization = "karthik_terraform"
+
+    workspaces {
+      name = "terrahouse"
+    }
+  }
 }
 
 provider "terratowns" {
@@ -13,24 +21,44 @@ provider "terratowns" {
   token     = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.teacherseat_user_uuid
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version     = var.content_version
-  assets_path         = var.assets_path
+module "home_bangalore_hosting" {
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.bangalore.public_path
+  content_version = var.bangalore.content_version
 }
 
-resource "terratowns_home" "home" {
-  name            = "How to play Arcanum in 2023!"
+resource "terratowns_home" "home_bangalore" {
+  name            = "Welcome to Bangalore - Silicon Valley of India"
   description     = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Bangalore, officially Bengaluru, is the capital and largest city of the southern Indian state of Karnataka.
+It has a population of more than 8 million and a metropolitan population of around 11 million, making it
+India's third most populous city and fifth most populous urban agglomeration, as well as South India's 
+second-largest urban agglomeration, and the 27th largest city in the world. Located on the Deccan Plateau,
+at a height of over 900 m (3,000 ft) above sea level, Bangalore has a pleasant climate throughout the year,
+with its parks and green spaces earning it the reputation of India's "Garden City".Its elevation is the highest
+among India's major cities
 DESCRIPTION
-  domain_name     = module.terrahouse_aws.cloudfront_url
-  town            = "missingo"
-  content_version = 1
+  domain_name     = module.home_bangalore_hosting.domain_name
+  town            = "the-nomad-pad"
+  content_version = var.bangalore.content_version
 }
+
+# module "home_max_hosting" {
+#   source          = "./modules/terrahome_aws"
+#   user_uuid       = var.teacherseat_user_uuid
+#   public_path     = var.max.public_path
+#   content_version = var.max.content_version
+# }
+
+# resource "terratowns_home" "home_max" {
+#   name            = "Formula 1 winners - Max Verstappen"
+#   description     = <<DESCRIPTION
+# Max Emilian Verstappen is a Belgian and Dutch racing driver
+# and the 2021, 2022, and 2023 Formula One World Champion. He competes
+# under the Dutch flag in Formula One with Red Bull Racing.
+# DESCRIPTION
+#   domain_name     = module.home_max_hosting.domain_name
+#   town            = "missingo"
+#   content_version = var.max.content_version
+# }
